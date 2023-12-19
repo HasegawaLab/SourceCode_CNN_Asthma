c8e7506835a052809985658bdfd4187e218dbf36
shibataryohei <shibataryohei@gmail.com>
2023-12-19 10:22:16 -0500

    Add self-defined functions

--- Function.R
+++ Function.R
@@ -1,3 +1,10 @@
-c2f
+c2f <- function(data){data %>% 
+    mutate_if(is.character, funs(as.factor(.)))}
 
-tidy_table
+tidy_table <- function(data){
+  data %>% 
+    mutate(p.value = format_pvalue(p.value)) %>% 
+    mutate_if(is.numeric, funs(sprintf("%.2f", .))) %>% 
+    mutate(Estimate = glue::glue("{estimate} ({conf.low}–{conf.high})")) %>% 
+    dplyr::select(-estimate, -std.error, -statistic,
+                  -conf.low, -conf.high)}
